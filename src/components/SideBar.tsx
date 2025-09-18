776772375a8cf7ca4874e1c128b3aa191fa52ac7
BrightNtt <nitiratnongyai@gmail.com>
2025-09-18 17:46:57 +0700

    chore[src]: re-organise sidebar menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,7 +5,9 @@ import {
   ShoppingBagIcon,
   BriefcaseIcon,
   DocumentTextIcon,
+  Bars3Icon,
 } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function SideBar() {
   const sidebarItems = [
@@ -15,26 +17,50 @@ export default function SideBar() {
     { name: "Customer", to: "/customers", icon: BriefcaseIcon },
     { name: "Reports", to: "/reports", icon: DocumentTextIcon },
   ];
+  const [isOpen, setIsOpen] = useState(true);
 
-  const renderedSidebarItems = sidebarItems.map((item) => {
-    const IconComponent = item.icon;
-    return (
-      <div>
-        <NavLink
-          key={item.to}
-          to={item.to}
-          className="flex items-center gap-2 py-3.5 px-3 rounded-lg text-gray-400 text-sm focus:bg-indigo-600 focus:text-white"
-        >
-          {IconComponent && <IconComponent className="h-5 w-5" />}
-          {item.name}
-        </NavLink>
-      </div>
-    );
-  });
   return (
-    <div className="flex flex-col min-w-[240px] p-3 bg-blue-50 border rounded-lg">
-      <h1 className="py-4 font-bold text-xl mb-[10px]">Dashboard</h1>
-      <nav className="flex flex-col gap-1">{renderedSidebarItems}</nav>
+    <div className={`flex flex-col items-center p-3 ${!isOpen && "p-0"}`}>
+      <div
+        className={`${
+          isOpen ? "w-[240px]" : "w-[50px]"
+        } bg-blue-50 rounded-lg transition-all ease-in-out duration-300`}
+      >
+        <div className="flex items-center px-2 py-4 font-bold text-xl mb-[10px]">
+          <button
+            className=" p-1 mr-[10px] hover:outline outline-gray-400 rounded-sm cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <Bars3Icon className="w-6 h-6 stroke-2" />
+          </button>
+          <span
+            className={`transition-opacity duration-200 ${
+              !isOpen ? "opacity-0 w-0 overflow-hidden" : "opacity-100"
+            }`}
+          >
+            Dashboard
+          </span>
+        </div>
+
+        <nav className="flex flex-col gap-1">
+          {sidebarItems.map(({ name, to, icon: Icon }) => (
+            <NavLink
+              key={to}
+              to={to}
+              className="flex items-center gap-2 text-gray-400 text-sm py-3.5 px-3"
+            >
+              <Icon className="size-6 stroke-2 shrink-0" />
+              <span
+                className={`${
+                  isOpen ? "opacity-100" : "opacity-0 w-0 overflow-hidden"
+                }`}
+              >
+                {name}
+              </span>
+            </NavLink>
+          ))}
+        </nav>
+      </div>
     </div>
   );
 }
