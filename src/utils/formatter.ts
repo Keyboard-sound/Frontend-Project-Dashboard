46da10fe4600994f700b526b44672cf15042871c
BrightNtt <nitiratnongyai@gmail.com>
2025-10-25 20:15:22 +0700

    feat[src]: create truncateTitle helper func

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -14,3 +14,18 @@ export const formatDate = (dateStr: string): string => {
 
   return `${day}-${month}`;
 };
+
+//
+export const truncateTitle = (title: string, maxLength: number): string => {
+  if (title.length <= maxLength) {
+    return title;
+  }
+  // Find the first whitespace AFTER maxLength index
+  const spaceAfterMax = title.indexOf(" ", maxLength);
+  if (spaceAfterMax !== -1) {
+    return title.slice(0, spaceAfterMax) + "...";
+  }
+
+  //if no space found
+  return title.slice(0, maxLength) + "...";
+};
