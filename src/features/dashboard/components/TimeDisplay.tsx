6e0d33f2f53ebaf364c0ab12ad0d1afdc6cfdefe
BrightNtt <nitiratnongyai@gmail.com>
2026-07-27 20:39:45 +0700

    refactor(hooks): move the time logic and formatTime to TimeDisplay.tsx and remove useWorldTime.ts

--- src/features/dashboard/components/TimeDisplay.tsx
+++ src/features/dashboard/components/TimeDisplay.tsx
@@ -1,12 +1,51 @@
-import useWorldTime from "@hooks/useWorldTime";
-import { formatTime } from "@hooks/useWorldTime";
+import { useEffect, useState } from "react";
 
 export default function TimeDisplay() {
-  const { time } = useWorldTime();
+  const [newTime, setNewTime] = useState<number | null>(null);
+
+  useEffect(() => {
+    setNewTime(Date.now());
+
+    const intervalID: ReturnType<typeof setInterval> = setInterval(() => {
+      setNewTime(Date.now());
+    }, 6000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  function formatTime(timestamp: number, timeZone: string = "Asia/Bangkok") {
+    if (!timestamp) {
+      console.error("timestamp missing");
+      return "";
+    }
+
+    const date = new Date(timestamp);
+    const timeStr = date.toLocaleString("en-US", {
+      timeZone,
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+    const day = date.getDate();
+    const daySuffix =
+      day % 10 === 1 && day !== 11
+        ? "st"
+        : day % 10 === 2 && day !== 12
+          ? "nd"
+          : day % 10 === 3 && day !== 13
+            ? "rd"
+            : "th";
+    const month = date.toLocaleString("en-US", { month: "long" });
+    const year = date.getFullYear();
+
+    return `${timeStr} at ${day}${daySuffix} ${month} ${year}`;
+  }
 
   return (
     <div className="pl-2 text-2xs lg:text-sm text-slate-400">
-      {time ? formatTime(new Date(time)) : formatTime(new Date())}
+      {newTime && formatTime(newTime)}
     </div>
   );
 }
