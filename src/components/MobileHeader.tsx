a9c10101789f68570306c17b29f8a9421705873a
BrightNtt <nitiratnongyai@gmail.com>
2026-07-29 23:05:07 +0700

    style(features): adjust mobile-header and add-product-button design

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -3,15 +3,13 @@ import type { FC } from "react";
 
 const MobileHeader: FC<{ onMenuClick: () => void }> = ({ onMenuClick }) => {
   return (
-    <div className="sticky top-0 z-50 lg:hidden">
-      <div className="flex justify-between items-center w-full h-14 pl-3 pr-1 py-2 font-bold bg-white shadow-sm">
-        <button
-          className="p-1 rounded-sm text-slate-400 active:bg-gray-100"
-          onClick={() => onMenuClick()}
-        >
-          <Bars3Icon className="w-5 h-5 stroke-2" />
-        </button>
-      </div>
+    <div className="flex justify-between items-center w-full h-9 pl-3 pr-1 py-2 font-bold bg-white shadow-sm">
+      <button
+        className="p-1 rounded-sm text-slate-400 active:bg-gray-100"
+        onClick={() => onMenuClick()}
+      >
+        <Bars3Icon className="w-5 h-5 stroke-2" />
+      </button>
     </div>
   );
 };
--- src/features/products/components/AddProductButton.tsx
+++ src/features/products/components/AddProductButton.tsx
@@ -12,7 +12,7 @@ export const AddProductButton: FC = () => {
       <button
         type="button"
         onClick={() => setIsDialogOpen(true)}
-        className="flex flex-row justify-between items-center gap-1 min-w-20 h-9 p-1 md:p-2 border border-gray-200 rounded-lg bg-white cursor-pointer hover:shadow-sm active:bg-gray-100"
+        className="flex flex-row justify-between items-center gap-1 min-w-20 h-8 p-1 md:p-2 border border-gray-200 rounded-lg bg-white cursor-pointer hover:shadow-sm active:bg-gray-100"
       >
         <PlusIcon className="size-3 text-slate-400" />
         <span className="text-2xs md:text-base font-normal text-slate-400">
