5b09d11f7f774f27a0eed1ed3df082eb406b63ae
BrightNtt <nitiratnongyai@gmail.com>
2026-07-30 22:31:04 +0700

    feat(schema): change price and stock field validation logic

--- src/features/products/product.schema.ts
+++ src/features/products/product.schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const ProductInputSchema = z.object({
   title: z.string().min(1, "Title is required"),
-  price: z.number().min(0, "Price must be greater than or equal to 0"),
+  price: z.number().min(1, "Price must be greater than 0"),
   description: z.string().optional(),
-  stock: z.number().min(0, "Stock must be greater than or equal to 0"),
+  stock: z.number().min(1, "Stock must be greater than 0"),
 });
 
 export type ProductInput = z.infer<typeof ProductInputSchema>;
