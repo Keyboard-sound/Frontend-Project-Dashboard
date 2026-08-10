b83a18b81bbe5167fbc1201126db4f56a2d87372
BrightNtt <nitiratnongyai@gmail.com>
2026-08-10 21:42:25 +0700

    refactor(components): remove create product dialog code

--- src/features/products/components/AddProductButton.tsx
+++ src/features/products/components/AddProductButton.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { PlusIcon } from "@heroicons/react/16/solid";
-import CreateProductForm from "./CreateProductForm";
 import type { FC } from "react";
 
-export const AddProductButton: FC = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+interface AddProductButtonProps {
+  onClick: () => void;
+}
+
+export const AddProductButton: FC<AddProductButtonProps> = ({ onClick }) => {
+  const handleClick = () => {
+    onClick();
+  };
 
   return (
     <>
       <button
         type="button"
-        onClick={() => setIsDialogOpen(true)}
+        onClick={handleClick}
         className="flex flex-row justify-between items-center h-6 lg:h-8 p-1 border border-gray-200 rounded-lg bg-white cursor-pointer hover:shadow-sm active:bg-gray-100"
       >
         <PlusIcon className="size-3 lg:size-4 text-slate-400" />
@@ -19,30 +22,6 @@ export const AddProductButton: FC = () => {
           Add Product
         </span>
       </button>
-
-      {/* Create product form */}
-      <Dialog
-        open={isDialogOpen}
-        onClose={setIsDialogOpen}
-        className="relative z-50"
-      >
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-black/50 transition duration-150 ease-out data-closed:opacity-0"
-          aria-hidden="true"
-        />
-        <div className="fixed inset-0 flex justify-end items-center">
-          <DialogPanel
-            transition
-            className="w-full max-w-md h-full rounded-sm bg-white transition duration-500 ease-in-out shadow-2xl data-closed:translate-x-full data-closed:opacity-0 data-open:translate-x-0"
-          >
-            <CreateProductForm
-              onSuccess={() => setIsDialogOpen(false)}
-              onCancel={() => setIsDialogOpen(false)}
-            />
-          </DialogPanel>
-        </div>
-      </Dialog>
     </>
   );
 };
