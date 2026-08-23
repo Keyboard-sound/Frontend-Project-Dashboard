a37ba04371594a61199305e71a43dae41c48fe34
BrightNtt <nitiratnongyai@gmail.com>
2026-08-23 21:30:56 +0700

    style(components): change edit dialog container width

--- src/features/products/components/EditProductDialog.tsx
+++ src/features/products/components/EditProductDialog.tsx
@@ -4,7 +4,7 @@ import type { FC } from "react";
 import useSalesStore from "@/store/useSalesStore";
 
 interface EditProductFormProps {
-  selectedId: number;
+  selectedId: number | null;
   isOpen: boolean;
   onClose: () => void;
 }
@@ -17,25 +17,22 @@ export const EditProductDialog: FC<EditProductFormProps> = ({
   const products = useSalesStore((state) => state.products);
   const selectedProductData = products.find((p) => p.id === selectedId);
 
-  console.log("selectedProduct ==>", selectedProductData);
-
   return (
     <Dialog className="relative z-50" open={isOpen} onClose={onClose}>
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-black/30 transition-opacity duration-150 ease-out data-closed:opacity-0"
-      >
-        <div className="fixed inset-0 flex justify-center items-center">
-          <DialogPanel className="w-full max-w-lg h-full md:h-[70%] rounded-md bg-white">
-            <ProductForm
-              mode="edit"
-              initialData={selectedProductData}
-              onSuccess={() => onClose()}
-              onCancel={() => onClose()}
-            />
-          </DialogPanel>
-        </div>
-      </DialogBackdrop>
+      />
+      <div className="fixed inset-0 flex justify-center items-center">
+        <DialogPanel className="w-full md:max-w-lg h-full md:h-[70%] rounded-md bg-white">
+          <ProductForm
+            mode="edit"
+            initialData={selectedProductData}
+            onSuccess={() => onClose()}
+            onCancel={() => onClose()}
+          />
+        </DialogPanel>
+      </div>
     </Dialog>
   );
 };
